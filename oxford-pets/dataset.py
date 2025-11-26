3cae3e1b5e2c6c0e252a58a79d5b39762e1906fb
gjoffre <gjoffre@insa-toulouse.fr>
2025-11-26 08:38:16 +0100

    working test for dataset loading

--- oxford-pets/dataset.py
+++ oxford-pets/dataset.py
@@ -4,7 +4,7 @@ from PIL import Image
 from pathlib import Path
 import torch
     
-def get_image_transforms():
+def get_image_transform():
     return transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
@@ -14,36 +14,46 @@ def get_image_transforms():
         ),
     ])
 
-def get_mask_transforms():
+def get_mask_transform():
     return transforms.Compose([
         transforms.Resize((224, 224), interpolation=Image.NEAREST),
         transforms.PILToTensor(),            # Keeps integer labels
     ])
 
 class OxfordPetsDataset(Dataset):
-    def __init__(self, transform = get_base_transforms(), images_dir = "./data/oxford-pets/images", masks_dir = "./data/oxford-pets/annotations/trimaps"):
-        self.images_dir = Path(images_dir)
-        self.masks_dir = Path(masks_dir)
+    def __init__(self, 
+            image_transform = get_image_transform(), 
+            image_dir = "./data/oxford-pets/images", 
+            mask_transform = get_mask_transform(),
+            mask_dir = "./data/oxford-pets/annotations/trimaps"
+        ):
         
-        self.transform = transform
-        self.images = sorted(list(self.images_dir.glob("*.jpg")))
+        self.image_dir = Path(image_dir)
+        self.mask_dir = Path(mask_dir)
+        
+        self.image_transform = image_transform or get_image_transform()
+        self.mask_transform  = mask_transform  or get_mask_transform()
+        
+        self.images = sorted(list(self.image_dir.glob("*.jpg")))
     
     def __len__(self):
         return len(self.images)
     
     def __getitem__(self, idx):
         img_path = self.images[idx]
-        mask_path = self.masks_dir / img_path.with_suffix(".png").name
+        mask_path = self.mask_dir / img_path.with_suffix(".png").name
 
         image = Image.open(img_path).convert("RGB")
-        mask = Image.open(mask_path)  # 1-channel
+        mask = Image.open(mask_path) # 1-channel
 
         # Convert mask to 0-background, 1-object
-        # mask = torch.tensor(np.array(mask), dtype=torch.long)
-        # mask = (mask == 1).long()  # or map 1=object, 2=border, 0=background
+        # mask = np.array(mask)
+        # mask = (mask == 1).astype(np.int64)
+        # mask = Image.fromarray(mask)
 
-        if self.transform:
-            image, mask = self.transform(image, mask)
+        # Apply transforms separately
+        image = self.image_transform(image)
+        mask = self.mask_transform(mask).squeeze(0)  # shape: (H, W)
 
         return image, mask
     
@@ -55,16 +65,20 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     
     full_dataset = OxfordPetsDataset()
-    display_loader = DataLoader(full_dataset, batch_size=8, shuffle=True)
-    images, labels = next(iter(display_loader))
-    print(labels)
-    print(images)
+    display_loader = DataLoader(full_dataset, batch_size=4, shuffle=True)
+    images, masks = next(iter(display_loader))
 
     plt.figure(figsize=(16, 4))
-    for i in range(N):
-        ax = plt.subplot(2, N//2, i+1)
-        img = unnormalize(images[i])
+    for i in range(4):
+        ax = plt.subplot(2, 4, i+1)
+        img = images[i].permute(1, 2, 0).numpy()
+        img = (img - img.min()) / (img.max() - img.min())   # quick unnormalize for display
         plt.imshow(img)
+        plt.axis("off")
+        
+        ax = plt.subplot(2, 4, i+5)
+        plt.imshow(masks[i], cmap="gray")
+        plt.title("Mask (Label)")
         plt.axis("off")
 
     plt.show()
